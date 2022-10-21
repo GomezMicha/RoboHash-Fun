2b64d38ea208e7d755fb082b17e3ebf307745b66
Micha <thomasagger@yahoo.co.uk>
2022-10-21 01:04:51 -0500

    Update

--- src/components/hash-images/hash-images.component.jsx
+++ src/components/hash-images/hash-images.component.jsx
@@ -4,10 +4,7 @@ import "./hash-images.style.css";
 export const HashImages = ({ name, changeAvatar }) => {
   return (
     <div className="avatar-image">
-      <img
-        src={`https://robohash.org/${name}${changeAvatar}&size=180x180`}
-        alt={name}
-      />
+      <img src={`https://robohash.org/${name}${changeAvatar}`} alt={name} />
     </div>
   );
 };
